2f89b9f6c42642d7c9db1904a918d03985f6d9ef
gordonfromgithub <gordonl2206@gmail.com>
2023-10-17 10:38:45 -0700

    fixed (mostly)

--- pleaseWork/Assets/Scenes/playerMovement.cs
+++ pleaseWork/Assets/Scenes/playerMovement.cs
@@ -7,8 +7,8 @@ public class playerMovement : MonoBehaviour
 {
     public float speed = 3f;
     public Rigidbody2D rb;
-    public Animator anim;
-    public SpriteRenderer Sprite;
+    //public Animator anim;
+   // public SpriteRenderer Sprite;
     private Vector2 dir;
     public bool hasKey = true;
 
@@ -27,48 +27,48 @@ public class playerMovement : MonoBehaviour
         dir = new Vector2(x, y).normalized; //where we move
         //transform.Translate(direction * Time.deltaTime * speed); //how we move
 
-        if (x > 0 || x < 0)
-        {
-            anim.SetTrigger("SideWays");
-            ClearWalkCycle();
-            anim.SetBool("Side", true);
-        }
+        //if (x > 0 || x < 0)
+        //{
+        //    anim.SetTrigger("SideWays");
+        //    ClearWalkCycle();
+       //     anim.SetBool("Side", true);
+       // }
 
-        else if (y > 0)
-        {
-            ClearWalkCycle();
-            anim.SetBool("Up", true);
-        }
+       // else if (y > 0)
+       // {
+       //     ClearWalkCycle();
+      //      anim.SetBool("Up", true);
+      //  }
 
-        else if (y < 0)
-        {
-            ClearWalkCycle();
-            anim.SetBool("Down", true);
-        }
+       // else if (y < 0)
+       // {
+       //     ClearWalkCycle();
+       //     anim.SetBool("Down", true);
+      //  }
 
-        else if (x == 0 && y == 0)
-        {
-            ClearWalkCycle();
-            anim.SetBool("Idle", true);
-        }
+      //  else if (x == 0 && y == 0)
+      //  {
+       //     ClearWalkCycle();
+       //     anim.SetBool("Idle", true);
+       // }
         
-        if (x < 0)
-        {
-            Sprite.flipX = true;
-        }
-        if (x > 0)
-        {
-            Sprite.flipX = false;
-        }
+      //  if (x < 0)
+      //  {
+      //      Sprite.flipX = true;
+     //   }
+     //   if (x > 0)
+     //   {
+      //      Sprite.flipX = false;
+     //   }
     }
 
-    public void ClearWalkCycle()
-    {
-        anim.SetBool("Down", false);
-        anim.SetBool("Idle", false);
-        anim.SetBool("Up", false);
-        anim.SetBool("Side", false);
-    }
+   //public void ClearWalkCycle()
+    //{
+    //    anim.SetBool("Down", false);
+   //     anim.SetBool("Idle", false);
+   //     anim.SetBool("Up", false);
+   //     anim.SetBool("Side", false);
+  //  }
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(dir.x * speed, dir.y * speed);
